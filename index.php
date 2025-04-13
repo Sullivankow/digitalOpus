f2f33e692c0ed3021cee2625535cdc05a6027fd6
SunDev <sundly@live.fr>
2025-04-13 16:21:17 +0200

    Update header

--- index.php
+++ index.php
@@ -4,18 +4,21 @@ require_once "template/header.php";
 ?>
 
 
+
 <div class="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 class="display-4 fw-bold text-body-emphasis"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Capture d'écran centrée</font></font></h1>
+    <h1 class="display-4 fw-bold text-body-emphasis">Capture d'écran centrée</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Concevez et personnalisez rapidement des sites mobiles réactifs avec Bootstrap, la boîte à outils open source front-end la plus populaire au monde, comprenant des variables et des mixins Sass, un système de grille réactif, de nombreux composants prédéfinis et de puissants plugins JavaScript.</font></font></p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Bouton principal</font></font></button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Secondaire</font></font></button>
-      </div>
+        <p class="lead mb-4">
+            Concevez et personnalisez rapidement des sites mobiles réactifs avec Bootstrap, la boîte à outils open source front-end la plus populaire au monde, comprenant des variables et des mixins Sass, un système de grille réactif, de nombreux composants prédéfinis et de puissants plugins JavaScript.
+        </p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+            <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Bouton principal</button>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondaire</button>
+        </div>
     </div>
     <div class="overflow-hidden" style="max-height: 30vh;">
-      <div class="container px-5">
-        <img src="bootstrap-docs.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Exemple d'image" width="700" height="500" loading="lazy">
-      </div>
+        <div class="container px-5">
+            <img src="bootstrap-docs.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Exemple d'image" width="700" height="500" loading="lazy">
+        </div>
     </div>
-  </div>
+</div>
