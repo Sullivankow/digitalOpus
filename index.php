e199831b2dc7fa895c0ff7dea4651d42ca96f060
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-04-17 23:44:08 +0200

    Update responsive mobile logo

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ require_once "template/header.php";
 
 <div class="container col-xxl-8 px-4 py-5">
   <div class="row flex-lg-row-reverse align-items-center g-5 py-5 data-aos=" data-aos="fade-up" data-aos-duration="1000">
-    <div class="col-10 col-sm-8 col-lg-6">
+    <div class="col-11 col-sm-8 col-lg-6 d-flex ">
       <img src="asset/images/logo1.png" class="d-block mx-auto img-fluid" alt="Logo" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
