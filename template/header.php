9cc7bdfb1184ce395b4a7cae2abfbf672ff6155b
SunDev <sundly@live.fr>
2025-07-20 15:47:10 +0200

    Update

--- template/header.php
+++ template/header.php
@@ -20,7 +20,7 @@
       <!-- Logo -->
       <a class="navbar-brand d-flex align-items-center" href="/">
         <img src="asset/images/logo4.png" alt="logo" width="100px" height="90px">
-        <span class="text-white ms-1 fs-4 fw-bold">Sundev Agency</span>
+        <span class="text-white ms-1 fs-4 fw-bold">SunDev Agency</span>
       </a>
 
       <!-- Bouton burger -->
