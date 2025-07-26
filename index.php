a84f9b7ba6ea569ef8068d3d54810f2416111d18
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-07-26 08:59:48 +0200

    Update reponsive mobile menu burger

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once "template/header.php";
-require_once "template/hero.php";
+
 require_once "template/aboutUs.php";
 require_once "template/realisations.php";
 require_once "template/whyChoose.php";
